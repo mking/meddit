94d125f5e91d381f78666dd2224a9d28c5aaac08
Matthew King <mking@users.noreply.github.com>
2018-08-27 20:00:22 -0700

    Add json encoder

--- app.py
+++ app.py
@@ -1,53 +1,88 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, Response
 import logging
 import http.client
 from redis import StrictRedis
 from uuid import uuid4
 from datetime import datetime
-from meddit.posts import PostCache
+from meddit.posts import PostCache, PostJSONEncoder
+import json
 
 http.client.HTTPConnection.debuglevel = 1
 logging.basicConfig(level=logging.DEBUG)
 
-cache = StrictRedis(host='localhost', port=6379, db=0)
+cache = StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 post_cache = PostCache(cache)
 app = Flask(__name__)
+app.json_encoder = PostJSONEncoder
+
+@app.errorhandler(400)
+def bad_request(e):
+    return Response(
+        response=json.dumps({
+            'success': False,
+            'message': e.description,
+        }),
+        status=400,
+        mimetype='application/json'
+    )
+
+@app.errorhandler(404)
+def not_found(e):
+    return Response(
+        response=json.dumps({
+            'success': False,
+            'message': e.description,
+        }),
+        status=404,
+        mimetype='application/json'
+    )
 
 @app.route('/posts', methods=['POST'])
-def create_post():
+def create():
     id = str(uuid4())
     author = request.get_json()['author']
-    created = datetime.now().timestamp()
+    gif_id = request.get_json()['gif_id']
+    created = int(datetime.now().timestamp())
     votes = 1
-    post_cache.create(id, author, created, votes)
+    post_cache.create(id, author, gif_id, created, votes)
     return jsonify({
         'success': True,
     })
 
-@app.route('/posts/:id', methods=['DELETE'])
-def delete_post():
-    id = request.args.get('id')
+@app.route('/posts/<id>', methods=['DELETE'])
+def delete(id):
+    if not post_cache.exists(id):
+        abort(404, 'Missing post')
+
     post_cache.delete(id)
     return jsonify({
         'success': True,
     })
 
-@app.route('/posts/:id', methods=['PUT'])
-def update_post():
-    id = request.args.get('id')
-    votes = request.get_json()['votes']
-    post_cache.update_votes(id, votes)
+@app.route('/posts/<id>/votes', methods=['PUT'])
+def update_votes(id):
+    amount = request.get_json()['amount']
+    if amount not in [-1, 1]:
+        abort(400, 'Invalid amount')
+
+    if not post_cache.exists(id):
+        abort(404, 'Missing post')
+
+    post_cache.update_votes(id, amount)
     return jsonify({
         'success': True,
     })
 
-@app.route('/posts/:id', methods=['GET'])
-def get_post():
-    id = request.args.get('id')
+@app.route('/posts/<id>', methods=['GET'])
+def get(id):
+    if not post_cache.exists(id):
+        abort(404, 'Missing post')
+
     post = post_cache.get(id)
     return jsonify(post)
 
 @app.route('/posts/recent', methods=['GET'])
-def get_recent():
-    posts = post_cache.get_recent()
+def recent():
+    limit = request.args.get('limit', 10)
+    posts = post_cache.recent(limit)
     return jsonify(posts)
